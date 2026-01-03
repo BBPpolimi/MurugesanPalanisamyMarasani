b46e7765dd02836aa2e477c159efd54f12bf78e7
Arziva <arunkumarnavamani@gmail.com>
2026-01-03 17:05:25 +0100

    Configure Google Maps API key in AppDelegate

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,8 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+    GMSServices.provideAPIKey("AIzaSyB0JGW9K_M69OPlEkUb4bjImj3ogpjJxNM")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
